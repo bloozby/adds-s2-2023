5d87ceb79f4ef30ecb846b34da0b8053418ded99
James Blazeby <blazebyjames@gmail.com>
2023-08-10 23:40:45 +0930

    all

---
+++ Reverser.h
@@ -0,0 +1,17 @@
+#ifndef REVERSER_H
+#define REVERSER_H
+
+#include <string>
+#include <iostream>
+
+class Reverser
+{
+private:
+    
+public:
+    Reverser();
+    int reverseDigit(int value);
+    std::string reverseString(std::string characters);
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 
 #include "Truckloads.h"
+#include "Reverser.h"
 #include <iostream>
 
 using namespace std;
 
 int main(){
-    Truckloads t;
-    int crates;
-    int load;
-    cout << "How many crates?" << endl << ": ";
-    cin >> crates;
-    cout << "how many crates per load?" << endl << ": ";
-    cin >> load;
-    int trucks = t.numTrucks(crates, load);
-    cout << "you will need " << trucks << " trucks.";
+    /*Truckloads t;
+    int trucks = t.numTrucks(15, 1);
+    cout << "for a 15 crates with a load size of 1" << endl;
+    cout << "you will need " << trucks << " trucks." << endl;*/
 
+    Reverser r;
+    int value;
+    cout << "enter a value: ";
+    cin >> value;
+    int rev = r.reverseDigit(value);
+    cout << "reversed: " << rev << endl;
+    cout << "enter a word: ";
+    string word;
+    cin >> word;
+    cout << "reversed: " << r.reverseString(word) << endl;
 
     return 0;
 }
