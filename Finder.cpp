eb9455cd1f55f269dfbc5b7536f7d358f94f2fc2
James Blazeby <blazebyjames@gmail.com>
2023-08-28 22:09:50 +0930

    removed equal

--- Finder.cpp
+++ Finder.cpp
@@ -2,15 +2,6 @@
 
 std::vector<int> Finder::findSubstrings(std::string s1, std::string s2) {
     std::vector<int> result;
-    if (s1 == s2)
-    {
-        for (size_t i = 0; i < s2.size(); i++)
-        {
-            result.push_back(1);
-        }
-        return result;
-    }
-    
 
     for(size_t i = 1; i <= s2.size(); i++) {
         size_t found = s1.find(s2, i);
