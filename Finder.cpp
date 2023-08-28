7049bcaddec617063ef593ca9d1444702c2b97cc
James Blazeby <blazebyjames@gmail.com>
2023-08-28 19:20:19 +0930

    no return else

--- Finder.cpp
+++ Finder.cpp
@@ -9,7 +9,7 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
         if (found != string::npos) {
             result.push_back(found);
         } else {
-            return result.push_back(-1);
+            result.push_back(-1);
         }
     }
     return result;
